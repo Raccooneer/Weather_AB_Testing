dec6fe0d9592690c8ccf9dd73a9ee3dc3e3e73e2
Racooneer <tinoherden@googlemail.com>
2021-08-01 16:30:20 +0200

    introduced function to tansform data and push to db in standardized form

--- weather_AB.py
+++ weather_AB.py
@@ -4,7 +4,7 @@
 #
 # Section 0: imports and credentials
 #   (1) package import
-#   (2) personal modul imports
+#   (2) personal module imports
 # 
 # ====================================================================================
 
@@ -15,17 +15,18 @@ import yaml
 import json
 from dotenv import load_dotenv
 
-# %% (2) personal modul imports
+# %% (2) personal module imports
 # for testing and debugging only
 # %load_ext autoreload
 # %autoreload 2
-
 from src.wrap_requests import get_multiple_to_file
+from src.wrap_psql import post_upsert_postgreSQL
 
 # %% [Markdown] ======================================================================
 #
 # Section 1: functions
 #   (1) save data from one provider to file
+#   (2) transform data and save to DB
 # 
 # ====================================================================================
 
@@ -54,13 +55,13 @@ def get_weatherdata_by_provider(provider = "meteostat"):
     with open("res/config/config.yaml", "r") as file:
         config = yaml.safe_load(file)
     
-    # escape if provider not in config or credentials
+    # escape if provider not in config
     if provider not in config.keys():
         raise f"Provider {provider} missing config"
 
     # setup vars
     start = pd.to_datetime("now").date() - + pd.Timedelta("1d")
-    end = start + pd.Timedelta("8d")
+    end = start + pd.Timedelta("9d")
 
     if config[provider]["type"] == "params":
         # create params to call
@@ -113,4 +114,66 @@ def get_weatherdata_by_provider(provider = "meteostat"):
         raise f"provider {provider} misses type"
 
     pass
+
+# %% (2) transform data and save to DB
+def transform_to_db(provider):
+    """Load and transform data of provider and push to DB in standardized form.
+    
+    args:
+        provider (str) : provider that has to be present in the config
+
+    return:
+        no return
+    """
+    # load config
+    with open("res/config/config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+    # escape if provider not in config
+    if provider not in config.keys():
+        raise f"Provider {provider} missing config"
+
+    # check if file exists
+    if not os.path.isfile(f"temp/{provider}.csv"):
+        raise f"this provider has no temporary file"
+
+    # load file
+    df = pd.read_csv(f"temp/{provider}.csv")
+
+    # special operations
+    if config[provider]["special_operations"] is not None:
+        # apply epoch to datetime (if required)
+        if "epoch_to_timestamp" in config[provider]["special_operations"]:
+            df = (
+                df.assign(timestamp = lambda x : pd.to_datetime(
+                    x.datetimeEpoch, 
+                    unit='s', 
+                    origin='unix', 
+                    utc = True))
+            )
+
+    # rename columns, filter and add create time
+    columns_to_save = [v for k, v in config[provider]["mapping"].items()] + ["city"]
+    df = (
+        df
+            .rename(columns=config[provider]["mapping"])
+            .filter(columns_to_save)
+            .assign(
+                create_time = pd.to_datetime("now", utc=True).floor("1H"),
+                provider = provider,
+                weather_time = lambda x : pd.to_datetime(x.weather_time, utc=True),
+                time_diff = lambda x : (x.weather_time.dt.date - x.create_time.dt.date).dt.days,
+                forecast_type = lambda x : x.time_diff.map(lambda x : 
+                    "actual"
+                    if x < 0
+                    else f"{x} day forecast"
+                )
+            )
+            .drop(["time_diff"], axis = 1)
+    )
+
+    # post data to db
+    post_upsert_postgreSQL("weather_forecast", df)
+
+    pass
+
 # %%
